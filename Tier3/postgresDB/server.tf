ef2c63cc8e2611fa7e28b891f006c1f37e8bb236
pradyumnaojha214 <80911553+pradyumnaojha214@users.noreply.github.com>
2022-12-15 01:55:12 +0530

    Update server.tf

--- Tier3/postgresDB/server.tf
+++ Tier3/postgresDB/server.tf
@@ -2,16 +2,15 @@ resource "azurerm_postgresql_server" "this" {
   name                             = var.name
   location                         = var.location
   resource_group_name              = var.resource_group_name
-  administrator_login              = var.pgsql_root_login_name
+  administrator_login              = var.administrator_login
   administrator_login_password     = var.administrator_login_password
-  sku_name                         = var.sql_sku
-  version                          = var.sql_version
-  backup_retention_days            = 7
-  geo_redundant_backup_enabled     = true
-  auto_grow_enabled                = true
+  sku_name                         = var.sku_name
+  version                          = var.version
+  backup_retention_days            = var.backup_retention_days
+  geo_redundant_backup_enabled     = var.geo_redundant_backup_enabled
+  auto_grow_enabled                = var.auto_grow_enabled
   public_network_access_enabled    = var.public_network_access_enabled
   ssl_enforcement_enabled          = var.ssl_enforcement_enabled
   ssl_minimal_tls_version_enforced = var.minimal_tls_version
-  tags                             = var.tags
   storage_mb                       = var.storage_mb
 }
