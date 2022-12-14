d11dc1f345426ca4cf724d4bfec14d1e6876446c
pradyumnaojha214 <80911553+pradyumnaojha214@users.noreply.github.com>
2022-12-15 03:22:22 +0530

    Add files via upload

---
+++ postgres.tf
@@ -0,0 +1,20 @@
+module postgre-db{
+    source              = "./Tier3/postgresDB"
+    name = "postgres"
+    location = local.resource_obj.location
+    resource_group_name = local.resource_obj.name
+    administrator_login = "admin"
+    administrator_login_password = "keep-secret-through-keyvault"
+    sku_name = "GP_Gen5_4"
+    Version = "11"
+    backup_retention_days = "7"
+    geo_redundant_backup_enabled = false
+    auto_grow_enabled = false
+    public_network_access_enabled = false
+    ssl_enforcement_enabled = true
+    ssl_minimal_tls_version_enforced = "TLS1_2"
+    storage_mb = 640000
+    charset = local.charset
+    collation = local.collation
+    subnet_id= local.subnet_id
+}
