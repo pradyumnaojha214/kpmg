0d8f9f6ce0ca98c42c746d8cfdba5c3bdca154f9
pradyumnaojha214 <80911553+pradyumnaojha214@users.noreply.github.com>
2022-12-15 01:36:44 +0530

    Update lb.tf

--- Tier1/loadBalancer/lb.tf
+++ Tier1/loadBalancer/lb.tf
@@ -4,22 +4,18 @@ resource "azurerm_public_ip" "public" {
   resource_group_name = var.name
   allocation_method   = var.allocation_method
   sku                 = var.sku
-
-  tags = var.tags
 }
 
 resource "azurerm_lb" "public" {
   name                = "${var.name}-public-lb"
   location            = var.location
-  resource_group_name = var.name
+  resource_group_name = var.resource_group_name
   sku                 = var.sku
 
   frontend_ip_configuration {
     name                 = "${var.name}-public-ip"
     public_ip_address_id = azurerm_public_ip.public.id
   }
-
-  tags = var.tags
 }
 
 resource "azurerm_lb_backend_address_pool" "public" {
@@ -28,31 +24,15 @@ resource "azurerm_lb_backend_address_pool" "public" {
 }
 
 resource "azurerm_lb_outbound_rule" "public" {
-  name                    = "OutboundRule"
+  name                    = "${var.name}-OutboundRule"
   resource_group_name     = var.name
   loadbalancer_id         = azurerm_lb.public.id
-  protocol                = "Tcp"
+  protocol                = var.protocol//"Tcp"
   backend_address_pool_id = azurerm_lb_backend_address_pool.public.id
 
   frontend_ip_configuration {
-    name = "${module.centralus_loadbalancer_labels.id_with_suffix.lb}-public-ip"
+    name = "${var.name}-public-ip"
   }
 }
 
-data "azurerm_virtual_network" "vm_vnet" {
-  name                = "centralus-fxe-data-mgmt-prd-fxconnect-vnet"
-  resource_group_name = "centralus-fxe-data-mgmt-prd-network-rg"
-}
 
-data "azurerm_network_interface" "backend_nic" {
-  for_each = local.lb_backend_vm_nic
-  name = each.key
-  resource_group_name = var.name
-}
-
-resource "azurerm_network_interface_backend_address_pool_association" "backend_nic" {
-  for_each = data.azurerm_network_interface.backend_nic
-  network_interface_id    = data.azurerm_network_interface.backend_nic[each.key].id
-  ip_configuration_name   = data.azurerm_network_interface.backend_nic[each.key].ip_configuration[0].name
-  backend_address_pool_id = azurerm_lb_backend_address_pool.public.id
-}
