f324e010bbafe55453e8dfa8f7d209432f748d74
pradyumnaojha214 <80911553+pradyumnaojha214@users.noreply.github.com>
2022-12-15 02:42:24 +0530

    Update penp.tf

--- Tier3/postgresDB/penp.tf
+++ Tier3/postgresDB/penp.tf
@@ -1,21 +1,14 @@
 resource "azurerm_private_endpoint" "this" {
-  for_each            = var.private_endpoints
-  name                = module.penp_labels.ids_with_suffix[format("%s-%s", var.name, each.key)].pendp
-  location            = azurerm_postgresql_server.this.location
+  name                = "${var.name}-pendp"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = each.value.subnet_id
-
-  private_dns_zone_group {
-    name                 = module.penp_labels.id
-    private_dns_zone_ids = [each.value.private_dns_zone_id]
-  }
+  subnet_id           = var.subnet_id
 
   private_service_connection {
-    name                           = module.penp_labels.ids_with_suffix[format("%s-%s", var.name, each.key)].pendp
+    name                           = "${var.name}-pendp"
     private_connection_resource_id = azurerm_postgresql_server.this.id
     subresource_names              = ["postgresqlServer"]
-    is_manual_connection           = each.value.is_manual_connection
+    is_manual_connection           = false
   }
 
-  tags = module.penp_labels.tags
 }
