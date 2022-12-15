5d5f1cc2d5ff173f82149d30a59de501d35c1503
pradyumnaojha214 <80911553+pradyumnaojha214@users.noreply.github.com>
2022-12-16 03:10:15 +0530

    Add files via upload

--- network/subnet/subnet.tf
+++ network/subnet/subnet.tf
@@ -1,14 +1,8 @@
-resource "azurerm_subnet" "envSubnets" {
+resource "azurerm_subnet" "this" {  ## block for creating subnet
   
   name                 = var.name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = [var.address_prefixes]
-  service_endpoints    = [
-    "Microsoft.AzureActiveDirectory",
-    "Microsoft.ContainerRegistry",
-    "Microsoft.KeyVault",
-    "Microsoft.Storage",
-    "Microsoft.Sql"
-  ]
+    
 }
